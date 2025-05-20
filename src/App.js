d5f508b8a36a58b845fcf92e1896fc06d4448e47
Andrew <abauer@chapman.edu>
2025-05-19 20:19:43 -0700

    Add Spiderman previews of movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import { useEffect } from 'react';
-
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import "./App.css";
+import SearchIcon from "./search.svg";
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 //REACT_APP_ must be the start of env variables to be recognized
 
+const movie1 = {
+  Title: "Shrek Retold",
+  Year: "2018",
+  imdbID: "tt9334162",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BZDY3ZDFjZWYtNDhmNC00OGVjLWIxZDYtNzlmYTAyYjMwNTcyXkEyXkFqcGdeQXVyNTk5NzczMDE@._V1_SX300.jpg",
+};
+
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
-    const searchMovies = async (title) => {
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-        console.log(data.Search);
-    }
+    setMovies(data.Search);
+  };
 
-    useEffect(() => {
-      searchMovies('Shrek');
-    }, []);
-    
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
-    return (
-        <h1>App</h1>
-    );
-}
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
